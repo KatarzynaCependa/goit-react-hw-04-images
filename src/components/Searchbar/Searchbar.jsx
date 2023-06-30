b46114946a742416ea304cd28f9842403d8c247d
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-06-30 18:36:07 +0200

    Refactoring Searchbar's component code

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,59 +1,43 @@
 import css from 'components/Searchbar/Searchbar.module.css';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export class Searchbar extends Component {
-  state = {
-    name: '',
-  };
+export const Searchbar = ({ onSubmit }) => {
+  const [name, setName] = useState('');
 
-  // przekazywane jest zdarzenie 'e'
-  handleSubmit = e => {
-    // aby zapobiec domyślnemu zachowaniu formularza (np. przeładowanie strony)
+  const handleSubmit = e => {
     e.preventDefault();
-    // za pomocą destrukturyzacji pobierana jest wartość pola 'name' ze stanu komponentu
-    const { name } = this.state;
-    // wartość pola 'name' jest przekazywana jako parametr do metody onSubmit, która jest przekazywana jako właściwość (props) komponentu Searchbar
-    this.props.onSubmit(name);
-    // stan komponentu jest aktualizowany, ustawiam 'name' na pusty string, aby wyczyścić pole wejściowe
-    this.setState({ name: '' });
+    onSubmit(name);
+    setName('');
   };
 
   // obsługa zmiany wartości pola wejściowego
-  handleChange = e => {
-    // pobieram wartość value ze zdarzenia e.target
+  const handleChange = e => {
     const { value } = e.target;
-    // aktualizuję 'name' w stanie komponentu
-    this.setState({ name: value });
+    setName(value);
   };
 
-  render() {
-    const { name } = this.state;
-    return (
-      <header className={css.Searchbar}>
-        {/* po wprowadzeniu wartości i kliknięciu "Search",
-        wartość jest przekazywana do komponentu App poprzez metodę onSubmit,
-        a pole wejściowe jest czyszczone */}
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchFormButton}>
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchFormButton}>
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={name}
-            // zmiana wartości wywołuje metodę handleChange
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={name}
+          // zmiana wartości wywołuje metodę handleChange
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
